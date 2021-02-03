8abc39a8d554a31185819a2144f9ffe9f7c918b5
momate <momate150@gmail.com>
2021-02-03 16:46:56 +0100

    Added custome exception

---
+++ src/main/java/com/momate/reminder/javaee/exception/UserNotFoundException.java
@@ -0,0 +1,9 @@
+package com.momate.reminder.javaee.exception;
+
+public class UserNotFoundException extends Exception{
+
+    public UserNotFoundException(String string) {
+        super(string);
+    }
+    
+}
